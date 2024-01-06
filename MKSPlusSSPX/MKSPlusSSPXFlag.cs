70605127caf73286058af9e09ec2608a0d3052ea
Shoey <shoeyfiller@gmail.com>
2024-01-06 18:27:43 -0500

    Add project files.

---
+++ MKSPlusSSPX/MKSPlusSSPXFlag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MKSPlusSSPX
+{
+    public class MKSPlusSSPXFlagStripper : PartModule
+    {
+
+        private List<MeshRenderer> rendererlist = new List<MeshRenderer>();
+
+        public void Start()
+        {
+            foreach (Transform t in part.FindModelTransforms("flagTransform"))
+            {
+                rendererlist.Add(t.GetComponent<MeshRenderer>());
+            }
+            if (rendererlist != null && rendererlist.Count > 0)
+            {
+                foreach (MeshRenderer r in rendererlist)
+                {
+                    r.enabled = false;
+                }
+            }
+        }
+    }
+}
